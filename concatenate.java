4d5fa7bf0bd035c96f35a5f2c757d48ff4d090f1
Soumalya <deresoumalya@gmail.com>
2026-01-29 12:46:36 +0530

    Add First java Program

---
+++ concatenate.java
@@ -0,0 +1,6 @@
+public class concatenate {
+    public static void main(String[] args) {
+        int a=5,b=6;
+        System.out.println("Concatenate = "+a+b);
+    }
+}
---
+++ demo.java
@@ -0,0 +1,12 @@
+
+
+
+class demo{
+
+     public static void main(String[] args) {
+        fun();
+    }
+    public static void fun() {
+        System.out.println("Hello, World!");
+    }
+}
