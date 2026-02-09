651ea88a08602fedc878443db000c6b0c4a7a50d
Soumalya <deresoumalya@gmail.com>
2026-02-09 23:38:12 +0530

    Add a java program using method

---
+++ Function.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Function {
+    int a=10;
+    void show(){
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        Function a1 = new Function();
+        a1.show();
+
+    }    
+}
